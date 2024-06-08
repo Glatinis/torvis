2648929083e1b2e09fd3c20a25b021c0a4a74077
BuildTools <rayanmaster50@gmail.com>
2024-06-09 03:56:50 +0500

    Basic backend for fetching torrent info added!

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-import fs from "fs";
-import bencode from "bencode";
+const tracker = require("./torrent-comm/tracker.js");
+const torrentParser = require("./torrent-comm/parser.js");
 
+const torrent = torrentParser.open("example.torrent");
 
-const txtDecoder = new TextDecoder();
-const rawTorrent = fs.readFileSync("example.torrent");
-const torrent = bencode.decode(rawTorrent);
-const announceUrl = txtDecoder.decode(torrent.announce);
-
-console.log(announceUrl);
+tracker.getInfo(torrent, info => {
+    console.log("information: ", info);
+});
